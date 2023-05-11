c9d3dcf1bf8fe743c521805eb6889ee0b06f90d7
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2023-05-11 13:32:03 -0400

    textInput

--- FirstProject/components/LoginScreen.js
+++ FirstProject/components/LoginScreen.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import { ScrollView, Text, StyleSheet, TextInput } from "react-native";
 
-export default function WelcomeScreen() {
-  const [firstName, onChangeFirstName] = useState("");
+export default function LoginScreen() {
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
+      <Text style={styles.regularText}>Login to continue </Text>
       <TextInput
         style={styles.inputBox}
-        value={firstName}
-        onChangeText={onChangeFirstName}
-        placeholder={"First Name"}
+        value={email}
+        onChangeText={onChangeEmail}
+        placeholder={"email"}
+        keyboardType={"email-address"}
+      />
+      <TextInput
+        style={styles.inputBox}
+        value={password}
+        onChangeText={onChangePassword}
+        placeholder={"password"}
+        keyboardType={"default"}
+        secureTextEntry={true}
       />
     </ScrollView>
   );
